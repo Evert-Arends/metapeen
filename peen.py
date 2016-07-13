28e3f6f7c53a3d51d168770cffc1f340d77ab014
Evert Arends <Evert-Arends@users.noreply.github.com>
2016-07-13 17:39:05 +0200

    Cleaned a bit

--- peen.py
+++ peen.py
@@ -1,16 +1,13 @@
 import requests
 
-JSONURL = 'https://metapeen.nl/hackflag/scoreboard.json'
+jsonUrl = 'https://metapeen.nl/hackflag/scoreboard.json'  # should be json (duh) and is the scoreboard url.
+
+
+def print_scores():
+    blob = requests.get(jsonUrl).json()
+
+    for key, value in sorted(blob.iteritems(), reverse=True, key=lambda (k, v): (v, k)):
+        print "%s: %s" % (key, value)
 
 if __name__ == "__main__":
-    pass
-
-class Peen:
-    def __init__(self):
-        self.getpeen()
-
-    def getpeen(self):
-        blob = requests.get('https://metapeen.nl/hackflag/scoreboard.json').json()
-
-        for key, value in sorted(blob.iteritems(), reverse=True, key=lambda (k, v): (v, k)):
-            print "%s: %s" % (key, value)
+    print_scores()
